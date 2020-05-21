021df633d6cd3c6af3b2073bcffec4d34b074036
witcherwjl <37329378+witcherwjl@users.noreply.github.com>
2020-05-21 20:58:40 +0800

    Revert "UDP一次传输"
    This reverts commit e225751df8b56a0b9c7c496db8b4a161edc6dc08.
    
    Signed-off-by: witcherwjl <37329378+witcherwjl@users.noreply.github.com>

---
+++ java_webBase/UDP/java_webBase/sendUDP.java
@@ -0,0 +1,43 @@
+package java_webBase;
+import java.net.*;
+import java.io.*;
+public class sendUDP {
+//    public static void main(String[] args) throws IOException {
+//        DatagramSocket ds = null;
+//        try {
+//            ds = new DatagramSocket();
+//            byte[] buf = "Hello World!".getBytes();
+//            InetAddress address = InetAddress.getByName("127.0.0.1");
+//            DatagramPacket dp = new DatagramPacket(buf,buf.length,address,10086);
+//            ds.send(dp);
+//        } catch (SocketException e) {
+//            e.printStackTrace();
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if(ds!=null)
+//                ds.close();
+//        }
+//    }
+	public static void main(String[] args) {
+		
+		try {
+			// 创建UDP套接字,不指定port
+			DatagramSocket ds = new DatagramSocket();
+			// 创建发送数组
+			byte[] buf = "HHHHH".getBytes();
+			// 给定地址
+			InetAddress address = InetAddress.getByName("127.0.0.1");
+			// 数据包对象
+			DatagramPacket dp = new DatagramPacket(buf, buf.length, address, 6666);
+			ds.send(dp);
+			ds.close();
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
